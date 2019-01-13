a865498f02f97f7618123b7fd5cf00c08864fc89
Nicholas Bauer <nicholasbauer@outlook.com>
2019-01-13 18:08:00 -0500

    Added test for printout of results.

---
+++ test/test_show.jl
@@ -0,0 +1,25 @@
+@testset "Display Tests" begin
+    @testset "AnovaData" begin
+        data = [AnovaValue( "Total", 1827.6975, 19),
+                AnovaResult(    "A",   70.3125,  1,   70.3125,  3.0706495,  0.098856175),
+                AnovaResult(    "B", 1386.1125,  1, 1386.1125, 60.533556,   7.9430782e-7),
+                AnovaResult("A × B",    4.9005,  1,    4.9005,  0.21401199, 0.64987001),
+                AnovaFactor(    "C",  366.372,  16,   22.89825),
+                AnovaFactor( "Error",    0,       0,  NaN)]
+        result = AnovaData(data)
+        expectedlines = ["",
+                         "Analysis of Variance Results",
+                         "",
+                         "Effect         SS  DF         MS          F           p",
+                         "-------------------------------------------------------",
+                         " Total  1827.7     19                                  ",
+                         "     A    70.3125   1    70.3125   3.07065   0.0988562 ",
+                         "     B  1386.11     1  1386.11    60.5336    7.94308e-7",
+                         " A × B     4.9005   1     4.9005   0.214012  0.64987   ",
+                         "     C   366.372   16    22.8983                       ",
+                         " Error     0.0      0                                  ",
+                         ""]
+
+        @test sprint(show, result) == join(expectedlines, "\n")
+    end
+end
