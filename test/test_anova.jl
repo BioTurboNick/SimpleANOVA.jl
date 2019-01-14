7dd7a02af3cab4c288c743bf609682444611c75d
Nicholas Bauer <nicholasbauer@outlook.com>
2019-01-13 23:01:04 -0500

    Update test_anova.jl

--- test/test_anova.jl
+++ test/test_anova.jl
@@ -248,7 +248,16 @@
         observations2 = cat(hcat([16.5, 18.4, 12.7, 14.0, 12.8], [39.1, 26.2, 21.3, 35.8, 40.2]),
                             hcat([14.5, 11.0, 10.8, 14.3, 10.0], [32.0, 23.8, 28.8, 25.0, 29.3]), dims = 3)
 
-        foreach(println(sum(c -> sum(c.^2), observations2)), 1:100)
+        println(sum(c -> sum(c.^2), observations2))
+        println(sum(c -> sum(c.^2), observations2))
+        println(sum(c -> sum(c.^2), observations2))
+        println(sum(c -> sum(c.^2), observations2))
+        println(sum(c -> sum(c.^2), observations2))
+        println(sum(c -> sum(c.^2), observations2))
+        println(sum(c -> sum(c.^2), observations2))
+        println(sum(c -> sum(c.^2), observations2))
+        println(sum(c -> sum(c.^2), observations2))
+        println(sum(c -> sum(c.^2), observations2))
     end
 
 #=
