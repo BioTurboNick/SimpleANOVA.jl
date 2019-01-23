8d0a1f6c8cb8214178cea27cbecb09d4e9298ddb
Nicholas Bauer <nicholasbauer@outlook.com>
2019-01-23 16:43:36 -0500

    Basic, unrefined, unfinished repeated-measures code

--- src/SimpleANOVA.jl
+++ src/SimpleANOVA.jl
@@ -38,35 +38,59 @@ end
 
 export anova, ftest, plot, levene
 
-#=
-function anovasubjectskernel()
+function rmanovakernel(observations, ncells, nreplicates)
     N = ncells * nreplicates
-    nfactors = nnestedfactors + ncrossedfactors
 
-    # collapse replicate dimension
     cellsums = eltype(observations) <: Number && nreplicates == 1 ? observations : sumfirstdim(observations)
     C = sum(cellsums) ^ 2 / N
     total = totalcalc(observations, N, C)
+    amongallnested, nestedsums, ncrossedfactorlevels, nnestedfactorlevels = amongnestedfactorscalc(cellsums, nfactorlevels, nnestedfactors, nreplicates, C)
+    cells = cellscalc(cellsums, nreplicates, ncells, C)
 
-    # for moment, assuming first dimension is subject
-    subjectsss = sum(sum(cellsums, dims = 2) .^ 2) / 3 -  C # 3 is number of elements for the subject, dims = 2 is all but the subject dimension
+    subjectsss =
+    subjectsdf = prod(nsubjectsfactorlevels) * nreplicates - 1
+    subjects = AnovaValue(subjectsss, subjectsdf)
 
-    #factors and interactions calculated as normal
+    # assumes all among factors and within factors are fixed, subjects is random
 
-    withinsubjectsss = totalss - subjectsss
+    # assumes that amongfactors are topmost
+    if namongfactors == 1
+        amongfactor = factorscalc(nestedsums, namongfactors, namongfactorlevels, N, C, amongfactornames) |> first
+        subjectswithin_amongfactor_ss = subjects.ss - amongfactor.ss
+        subjectswithin_amongfactor_df = subjects.df - amongfactor.df
+        subjectswithin_amongfactor = AnovaFactor(subjectswithin_amongfactor_ss, subjectswithin_amongfactor_df)
+    elseif namongfactors == 2
+        amongfactors = factorscalc(nestedsums, namongfactors, namongfactorlevels, N, C, amongfactornames)
+        amonginteractions, amonginteractionsmap = interactionscalc(cells, nestedsums, amongfactors, namongfactors, namongfactorlevels, nnestedfactorlevels, nreplicates, C, amongfactornames) # 9 kb allocated here!
+        subjectswithin_amongfactor_ss = subjects.ss - sum(f -> f.ss, amongfactors) - amonginteractionsmap[(1,2)].ss
+        subjectswithin_amongfactor_df = subjects.df - sum(f -> f.df, amongfactors) - amonginteractionsmap[(1,2)].df
+        subjectswithin_amongfactor = AnovaFactor(subjectswithin_amongfactor_ss, subjectswithin_amongfactor_df)
+    end
 
-    withinsubjectsdf = nsubjects * nfactoroutside * factorinsidedf
-    subjectswithinfactorsss = subjectss - factorsss  #guess
-    subjectswithinfactorsdf = subjectdf - factorsdf
+    withinsubjectsss = total.ss - subjects.ss
+    withinsubjectsdf = total.df - subjects.df
+    withinsubjects = AnovaValue(withinsubjectsss, withinsubjectsdf)
 
-    withinsubjectinteractionsss = withinsubjectsss - withinsubjectsfactorss - interactionss
-    withinsubjectinteractionsdf = withisubjectsdf - withinsubjectsfactordf - interactiondf
+    # needs to adjust for not being topmost factors
+    if nwithinfactors == 1
+        withinfactor = factorscalc(nestedsums, nwithinfactors, nwithinfactorlevels, N, C, withinfactornames)
+        factor_subjectswithin_interaction_df = subjectswithin_amongfactor_df * withinfactor.df
+        factor_subjectswithin_interaction_ss = ?????????
+    elseif nwithinfactors == 2
+        withinfactors = factorscalc(nestedsums, nwithinfactors, nwithinfactorlevels, N, C, withinfactornames)
+        withininteractions, withininteractionsmap = interactionscalc(cells, nestedsums, withinfactors, nwithinfactors, nwithinfactorlevels, nnestedfactorlevels, nreplicates, C, withinfactornames) # 9 kb allocated here!
+        factor_subjectswithin_interaction_df = subjectswithin_amongfactor_df .* [f.df for f in withinfactors]
+        factor_subjectswithin_interaction_ss = ?????????
+    end
 
+    if namongfactors + nwithinfactors == 3
+        interaction_subjectswithin_df = subjectswithin_amongfactor.df .* prod(f -> f.df, withinfactors)
+        interaction_subjectswithin_ss = ?????????
+    end
 
-    # test for B; B / withinsubjectinteractions
-    #test for A; A / subjects within factor A
-    # test for interaction; interaction / withinsubjectinteraction
+    amongsubjectsdenominator = subjectswithin_amongfactor
+    withinsubjectsdenominators .= pairinteractionsubjectswithinms, threeinteractionsubjectswithinms
 end
-=#
+
 
 end
