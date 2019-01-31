aa2035d261f0eeffc456fad84758d2a0f1e1b847
Nicholas Bauer <nicholasbauer@outlook.com>
2019-01-31 13:40:57 -0500

    Pairwise plots now generated

--- src/anova_plots.jl
+++ src/anova_plots.jl
@@ -9,18 +9,31 @@ Creates factor plots for ANOVA data.
 
 Enables inspection of ANOVA for the effect size of interactions and relationships between factor levels.
 
-Currently limited to 2 factors. Reference Plots package to use this.
+Import Plots package to use this.
 """
 function plot(anova::AnovaData)
     nfactors = length(anova.crossedfactors)
-    if nfactors > 2
-        error("Not yet able to handle more than 2 factors.")
+    if nfactors == 1
+        error("Not useful to plot just one factor.")
     end
     nfactorlevels = size(anova.cellmeans)
-    plot(legend = false)
-    foreach(i -> plot!(results.cellmeans[:,i], seriestype=:line), 1:nfactorlevels[2])
-    factorAmidpoint = (1 + nfactorlevels[1]) / 2
-    factormeans = [mean(anova.cellmeans, dims = (1:nfactors)[Not(i)]) for i = 1:nfactors]
+    plots = Array{Plots.Plot, 2}(undef, nfactors, nfactors)
+    for i in 1:nfactors
+        for j in (1:nfactors)[Not(i)]
+            plots[i,j] = plot([selectdim(anova.cellmeans, j, k) for k = 1:nfactorlevels[j]], legend = false)
+        end
+        plots[i,i] = plot()
+        factormidpoint = (1 + nfactorlevels[i]) / 2
+    end
+    plot(plots..., layout = (nfactors, nfactors))
+#=
+    factormeans = [mean(anova.cellmeans, dims = (1:nfactors)[Not(i)]) |> vec for i = 1:nfactors]
+
+
+    plot!(factormeans, layout = (nfactors, nfactors), seriestype=:scatter)
+    plot!(repeat([factormidpoint], nfactorlevels), factormeans[2]', seriestype=:scatter)
+
     plot!(factormeans[1], seriestype=:scatter)
-    plot!(repeat([factorAmidpoint], nfactorlevels[2]), factormeans[2]', seriestype=:scatter)
+    plot!(repeat([factorAmidpoint], nfactorlevels[2]), factormeans[2], seriestype=:scatter)
+    =#
 end
