f8ee7e56c2b697776e01e3b8cda9d438ab423cd8
Nicholas Bauer <nicholasbauer@outlook.com>
2019-08-16 12:57:57 -0400

    Added basic Levene's Test
    Not ideal, but works.

---
+++ src/pretests.jl
@@ -0,0 +1,44 @@
+function levene(observations::AbstractArray{T}, factortypes::Vector{FactorType} = FactorType[]; factornames::Vector{<:AbstractString} = String[], hasreplicates = true) where {T <: Union{Number, AbstractVector{<:Number}}}
+    length(observations) > 0 || return
+
+    firstlevelreplicates = eltype(observations) <: Number ? hasreplicates : false
+    nfactors = ndims(observations) - (firstlevelreplicates ? 1 : 0)
+
+    # defaults to assuming all unspecified factors are fixed
+    if length(factortypes) < nfactors
+        nremaining = nfactors - length(factortypes)
+        append!(factortypes, repeat([fixed], nremaining))
+    end
+
+    validate(factortypes, factornames, nfactors)
+
+    # automatically assigns alphabetical names if not provided.
+    if isempty(factornames)
+        factornames = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"][1:nfactors]
+        reverse!(factornames)
+    end
+
+    nnestedfactors = count(f -> f == nested, factortypes)
+    crossedfactortypes = filter(f -> f ∈ [fixed, random], factortypes)
+    reverse!(crossedfactortypes)
+    ncrossedfactors = length(crossedfactortypes)
+    ncrossedfactors < 4 || error("ANOVA with 4 or more crossed factors is not supported.")
+    nreplicates = firstlevelreplicates ? size(observations, 1) : length(observations[1])
+    firstlevelreplicates || all(c -> length(c) == nreplicates, observations) || throw(ErrorException("All cells must have the same number of replicates."))
+    ncells = Int.(length(observations) / (firstlevelreplicates ? nreplicates : 1))
+    nfactorlevels = firstlevelreplicates ? [size(observations)...][Not(1)] : [size(observations)...]
+
+    crossedfactornames = factornames[factortypes .≠ nested]
+    nestedfactornames = factornames[factortypes .== nested]
+
+    levenekernel(observations, nreplicates, ncells, nnestedfactors, ncrossedfactors, nfactorlevels, crossedfactortypes, crossedfactornames, nestedfactornames)
+end
+
+function levenekernel(observations, nreplicates, ncells, nnestedfactors, ncrossedfactors, nfactorlevels, crossedfactortypes, crossedfactornames, nestedfactornames)
+    cellsums = eltype(observations) <: Number && nreplicates == 1 ? observations : sumfirstdim(observations)
+    cellmeans = cellsums ./ nreplicates
+    absdevs = abs.(observations .- cellmeans')
+    anovakernel(absdevs, nreplicates, ncells, nnestedfactors, ncrossedfactors, nfactorlevels, crossedfactortypes, crossedfactornames, nestedfactornames)
+end
+
+export levene
