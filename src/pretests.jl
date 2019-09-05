6811bc23bfe1809c72afb23446045e37e1bdf88a
Nicholas Bauer <nicholasbauer@outlook.com>
2019-09-05 13:21:58 -0400

    Refined Levene function and added vector-assignment version

--- src/pretests.jl
+++ src/pretests.jl
@@ -1,46 +1,60 @@
-function levene(observations::AbstractArray{T}, factortypes::Vector{FactorType} = FactorType[]; factornames::Vector{<:AbstractString} = String[], hasreplicates = true) where {T <: Union{Number, AbstractVector{<:Number}}}
+function levene(observations::AbstractArray{T}) where {T <: Union{Number, AbstractVector{<:Number}}}
     length(observations) > 0 || return
 
-    firstlevelreplicates = eltype(observations) <: Number ? hasreplicates : false
+    firstlevelreplicates = eltype(observations) <: Number ? true : false
     nfactors = ndims(observations) - (firstlevelreplicates ? 1 : 0)
 
-    # defaults to assuming all unspecified factors are fixed
-    if length(factortypes) < nfactors
-        nremaining = nfactors - length(factortypes)
-        append!(factortypes, repeat([fixed], nremaining))
-    end
-
-    validate(factortypes, factornames, nfactors)
-
-    # automatically assigns alphabetical names if not provided.
-    if isempty(factornames)
-        factornames = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"][1:nfactors]
-        reverse!(factornames)
-    end
-
-    nnestedfactors = count(f -> f == nested, factortypes)
-    crossedfactortypes = filter(f -> f ∈ [fixed, random], factortypes)
-    reverse!(crossedfactortypes)
-    ncrossedfactors = length(crossedfactortypes)
-    ncrossedfactors < 4 || error("ANOVA with 4 or more crossed factors is not supported.")
     nreplicates = firstlevelreplicates ? size(observations, 1) : length(observations[1])
+    nreplicates > 1 || throw(ErrorException("Levene's test is invalid if there are no replicates."))
     firstlevelreplicates || all(c -> length(c) == nreplicates, observations) || throw(ErrorException("All cells must have the same number of replicates."))
+
     ncells = Int.(length(observations) / (firstlevelreplicates ? nreplicates : 1))
     nfactorlevels = firstlevelreplicates ? [size(observations)...][Not(1)] : [size(observations)...]
 
-    crossedfactornames = factornames[factortypes .≠ nested]
-    nestedfactornames = factornames[factortypes .== nested]
+    observations = firstlevelreplicates ? reshape(observations, nreplicates, ncells) : reshape(observations, ncells)
+    levenekernel(observations, nreplicates, ncells)
+end
 
-    levenekernel(observations, nreplicates, ncells, nnestedfactors, ncrossedfactors, nfactorlevels, crossedfactortypes, crossedfactornames, nestedfactornames)
+function levene(observations::AbstractVector{T}, factorassignments::AbstractVector{<:AbstractVector}) where {T <: Number}
+    length(observations) > 0 || return
+    nfactors = length(factorassignments)
+    N = length(observations)
+    N % nfactors == 0 || error("Design is unbalanced.")
+    all(length.(factorassignments) .== N) || error("Each observation must have an assignment for each factor.")
+
+    factorlevels = factorassignments .|> unique .|> sort
+    nfactorlevels = length.(factorlevels)
+    all(N .& nfactorlevels .== 0) || error("Design is unbalanced.")
+    factorlevelcounts = [[count(l -> l == factorlevels[i][j], factorassignments[i]) for j ∈ 1:nfactorlevels[i]] for i ∈ 1:nfactors]
+    nperfactorlevel = factorlevelcounts .|> unique
+    all(nperfactorlevel .|> length .== 1) || error("Design is unbalanced.")
+    nperfactorlevel = nperfactorlevel .|> first
+
+    if !(isa(factorassignments, Number)) || any(maximum.(factorlevels) .> nfactorlevels)
+        compressedfactorlevels = [1:i for i ∈ nfactorlevels]
+        factorlevelremapping = [factorlevels[i] .=> compressedfactorlevels[i] for i ∈ 1:nfactors]
+        factorassignments = [replace(factorassignments[i], factorlevelremapping[i]...) for i ∈ 1:nfactors]
+    end
+
+    #deduplicate above
+
+    ncells = prod(nfactorlevels)
+    nreplicates = Int(N / ncells)
+
+    nlevels = [nreplicates; ncells]
+    sortorder = sortperm(repeat(1:nreplicates, Int(N / nreplicates)) .+
+                         sum([factorassignments[i] .* prod(nlevels[1:i]) for i ∈ 1:nfactors]))
+    observationsmatrix = reshape(observations[sortorder], nlevels...)
+    levenekernel(observationsmatrix, nreplicates, ncells)
 end
 
 # need median version
 
-function levenekernel(observations, nreplicates, ncells, nnestedfactors, ncrossedfactors, nfactorlevels, crossedfactortypes, crossedfactornames, nestedfactornames)
-    cellsums = eltype(observations) <: Number && nreplicates == 1 ? observations : sumfirstdim(observations)
+function levenekernel(observations, nreplicates, ncells)
+    cellsums = sumfirstdim(observations)
     cellmeans = cellsums ./ nreplicates
     absdevs = abs.(observations .- cellmeans')
-    anovakernel(absdevs, nreplicates, ncells, nnestedfactors, ncrossedfactors, nfactorlevels, crossedfactortypes, crossedfactornames, nestedfactornames)
+    anovakernel(absdevs, nreplicates, ncells, 0, 1, ncells, [fixed], ["Groups"], String[])
 end
 
 export levene
