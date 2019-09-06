fcc24a1e292d6449334f5ad81baa9b181ca9d1e2
Nicholas Bauer <nicholasbauer@outlook.com>
2019-09-06 16:01:36 -0400

    DataFrames.jl deprecation change

--- src/pretests_dataframes.jl
+++ src/pretests_dataframes.jl
@@ -1,9 +1,8 @@
 using .DataFrames
 
 function levene(df::DataFrame, observationscolumn::Symbol, factorcolumns::Vector{Symbol})
-    observations = df[observationscolumn]
+    observations = df[!, observationscolumn]
     length(observations) > 0 || return
     eltype(observations) <: Number || error("Obervations must be numeric")
-    isempty(factornames) && (factornames = [String(col) for col ∈ factorcolumns])
-    levene(df[observationscolumn], [df[x] for x ∈ factorcolumns])
+    levene(df[!, observationscolumn], [df[!, x] for x ∈ factorcolumns])
 end
